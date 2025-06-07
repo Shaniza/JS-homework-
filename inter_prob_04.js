43c2fd9f8615ec175bbcdaeb4e8e4a2a71b32e8b
Shaniza <shaniza.rr17@gmail.com>
2025-06-07 23:36:47 +0600

    intermediate code done

---
+++ inter_prob_04.js
@@ -0,0 +1,14 @@
+//4. Write a function that flattens a nested array.
+function flattenArray(arr){
+    let result = []
+    for(let i of arr){
+        if(Array.isArray(i)){
+            result = result.concat(flattenArray(i))
+            }else{
+                result.push(i)
+            }
+    }
+    return result
+
+}
+console.log(flattenArray([1, [2, [3, 4], 5], 6]))
